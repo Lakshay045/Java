c628a40da9fa19d118243f193ebfadde8015d653
Lakshay Mittal <Lakshaymittal709@gmail.com>
2026-08-20 01:26:21 +0530

    Completed While Loop

---
+++ Java/06_Loops/While_loop/LoopVariables.java
@@ -0,0 +1,21 @@
+public class LoopVariables {
+
+    public static void main(String args[]) {
+
+        int i = 1;
+
+        while (i <= 4) {
+
+            int j = 1;
+
+            while (j <= 4) {
+
+                System.out.println("i = " + i + " j = " + j);
+
+                j++;
+            }
+
+            i++;
+        }
+    }
+}
