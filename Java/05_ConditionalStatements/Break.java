b381aa1067d12a085cde924ae46cc1f746f6830f
Lakshay Mittal <Lakshaymittal709@gmail.com>
2026-08-19 21:40:59 +0530

    Added New Statement

---
+++ Java/05_ConditionalStatements/Break.java
@@ -0,0 +1,21 @@
+public class Break {
+
+    public static void main(String args[]) {
+
+        int day = 2;
+
+        switch (day) {
+
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            default:
+                System.out.println("Invalid Day");
+        }
+    }
+}
