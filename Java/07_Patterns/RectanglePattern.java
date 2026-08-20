f4d2b6cb76eae34c31933930229cffd25cc065e3
Lakshay Mittal <Lakshaymittal709@gmail.com>
2026-08-20 16:48:42 +0530

    Started Patterns

---
+++ Java/07_Patterns/RectanglePattern.java
@@ -0,0 +1,19 @@
+public class RectanglePattern {
+
+    public static void main(String args[]){
+
+       
+
+        for(int row= 1 ; row <= 4 ; row++){
+            for(int col = 1 ; col <= 5 ; col++){
+                System.out.print("* ");
+            }
+
+            // Move to next row
+            
+            System.out.println();
+
+        }
+    }
+    
+}
